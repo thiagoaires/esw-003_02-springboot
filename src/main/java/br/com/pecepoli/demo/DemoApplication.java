d0698a06ffde15cdaea01f8b774af01c8af212d8
Thiago Aires <makarrao@gmail.com>
2024-06-03 21:37:58 -0300

    nome de alunos no 'main.java'

--- src/main/java/br/com/pecepoli/demo/DemoApplication.java
+++ src/main/java/br/com/pecepoli/demo/DemoApplication.java
@@ -3,6 +3,12 @@ package br.com.pecepoli.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Alunos:
+// Allan Yves
+// Gustavo Nogueira
+// Marcos Lima
+// Thiago Aires
+
 @SpringBootApplication
 public class DemoApplication {
 
